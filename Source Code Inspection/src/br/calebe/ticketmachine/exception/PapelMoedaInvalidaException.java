419a5ed583c258c87aacceb102c3fbe256975f55
Nicole Martins dos Santos <58263460+nicolemartins7@users.noreply.github.com>
2024-03-06 20:44:25 -0300

    Update PapelMoedaInvalidaException.java

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -1,9 +1,11 @@
+// Nicole Martins dos Santos - 100365577
+
 package br.calebe.ticketmachine.exception;
 
 /**
  *
  * @author Calebe de Paula Bianchini
  */
-public class PapelMoedaInvalidaException extends Exception {
+public class PapelMoedaInvalidaException extends Exception { // falta a implementação da lógica da classe
     
 }
